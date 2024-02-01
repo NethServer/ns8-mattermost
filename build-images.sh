2258fcbece7859d1c44380b91c011477a844f4e1
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-02-01 10:34:33 +0100

    Add php83-xmlreader and php83-xmlwriter dependencies

--- build-images.sh
+++ build-images.sh
@@ -61,7 +61,10 @@ apk add --no-cache \
   php83-pdo_pgsql \
   php83-pdo \
   php83-session \
-  php83-xml
+  php83-xml \
+  php83-xmlreader \
+  php83-xmlwriter
+
 (
   # dowload code from github
   wget https://github.com/Crivaledaz/Mattermost-LDAP/archive/refs/tags/v${MATTERMOST_LDAP}.tar.gz -O /tmp/mattermost-ldap.tar.gz
