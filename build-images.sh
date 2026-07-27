afc3a85f18a93b146488afb99f92d177c6f6a19a
Andrea Leardini <andrea.leardini@nethesis.it>
2026-07-27 09:18:58 +0200

    build: require core >= 3.20.1

--- build-images.sh
+++ build-images.sh
@@ -32,7 +32,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.tcp-ports-demand=2" \
     --label="org.nethserver.udp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.min-core=3.12.4-0" \
+    --label="org.nethserver.min-core=3.20.1" \
     --label="org.nethserver.images=docker.io/postgres:17.10-alpine docker.io/mattermost/mattermost-team-edition:10.11.22" \
     "${container}"
 # Commit the image
